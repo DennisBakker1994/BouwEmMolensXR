70fb6dcded3d3f410c81e85bd94e7dbb4a692567
MitchelBroekate <mitchelb11@outlook.com>
2024-03-11 16:03:22 +0100

    Main menu layout done

---
+++ Assets/Scripts/UI/ButtonMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ButtonMenu : MonoBehaviour
+{
+    
+    public void Play()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
