7b1f42f58928ece933fd8625df382c7bd5f09346
MacKenzie1976 <97097945@st.deltion.nl>
2024-03-12 16:48:34 +0100

    Movement
    Hopla :3

---
+++ Assets/Scripts/Movement/DefaultMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultMovement : MonoBehaviour
+{
+    private Movement controls;
+    private Vector2 move;
+    public CharacterController character;
+    public float moveSpeed = 1f; 
+
+
+
+    void Start()
+    {
+        controls = new Movement();
+    }
+
+    public void PlayerMovement()
+    {
+        move = controls.Player.ZXMovement.ReadValue<Vector2>();
+        Vector3 movement = (move.y * transform.forward) + (move.x * transform.right);
+        character.Move(movement * moveSpeed * Time.deltaTime);
+
+    }
+    void Update()
+    {
+        
+    }
+}
