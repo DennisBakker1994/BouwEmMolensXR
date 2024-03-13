97d6fa4db6804760c0a0b80b216c0bcb5aa6f35b
MacKenzie1976 <97097945@st.deltion.nl>
2024-03-13 15:31:59 +0100

    Up Down Movement

--- Assets/Scripts/Movement/DefaultMovementY.cs
+++ Assets/Scripts/Movement/DefaultMovementY.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UIElements;
 
 public class DefaultMovementY : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Movement controls;
+    private float move = 0;
+    public CharacterController character;
+    public float moveSpeed;
+
+    public void Start()
+    {
+        controls = new Movement();
+    }
+
+    public void PlayerUpDown()
+    {
+        move = controls.Player.YMovement.ReadValue<float>();
+    }
+
+    public void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
+
 }
+//private Movement controls;
+//private Vector2 move;
+//public CharacterController character;
+//public float moveSpeed = 1f;
+
+
+
+//void Start()
+//{
+ //   controls = new Movement();
+//}
+
+//public void PlayerMovement()
+//{
+ //   move = controls.Player.ZXMovement.ReadValue<Vector2>();
+ //   Vector3 movement = (move.y * transform.up) + (move.x * transform.right);
+ //   character.Move(movement * moveSpeed * Time.deltaTime);
+
+//}
