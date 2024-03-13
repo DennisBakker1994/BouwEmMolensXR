fa28d396e75763ea3c2132e15eae29d2bc0c8920
MitchelBroekate <mitchelb11@outlook.com>
2024-03-13 14:47:57 +0100

    dirty way

--- Assets/Scripts/UI/BuildingCycle.cs
+++ Assets/Scripts/UI/BuildingCycle.cs
@@ -6,16 +6,24 @@ public class BuildingCycle : MonoBehaviour
 {
     public GameObject[] builds;
 
+    public GameObject build1;
+    public GameObject build2;
+    public GameObject build3;
+    public GameObject build4;
+    public GameObject build5;
+    public GameObject build6;
+
     int buildingStyle;
 
     public void ChangeIntPlus()
     {
-        buildingStyle++;
 
-        if (buildingStyle > 5)
+        if (buildingStyle == 5)
         {
             buildingStyle = 0;
         }
+
+        buildingStyle++;
 
         SetAllToFalse();
         builds[buildingStyle].SetActive(true);
@@ -23,7 +31,6 @@ public class BuildingCycle : MonoBehaviour
 
     public void ChangeIntMinus()
     {
-        buildingStyle--;
 
         if (buildingStyle < 0)
         {
@@ -31,15 +38,19 @@ public class BuildingCycle : MonoBehaviour
 
         }
 
+        buildingStyle--;
+
         SetAllToFalse();
         builds[buildingStyle].SetActive(true);
     }
 
     void SetAllToFalse()
     {
-        for (int i = 0; i <= builds.Length; i++)
-        {
-            builds[i - 1].SetActive(false);
-        }
+        build1.SetActive(false);
+        build2.SetActive(false);
+        build3.SetActive(false);
+        build4.SetActive(false);
+        build5.SetActive(false);
+        build6.SetActive(false);
     }
 }
