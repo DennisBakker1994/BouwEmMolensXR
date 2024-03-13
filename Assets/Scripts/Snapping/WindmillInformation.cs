74488964f4875088373a38454446a26212e192c9
Dennis Bakker <dennis--bakker@hotmail.com>
2024-03-13 16:44:12 +0100

    Snap System

--- Assets/Scripts/Snapping/WindmillInformation.cs
+++ Assets/Scripts/Snapping/WindmillInformation.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindmillSnapCheck : MonoBehaviour
+
+public class WindmillInformation : MonoBehaviour
 {
     public enum Part
     {
