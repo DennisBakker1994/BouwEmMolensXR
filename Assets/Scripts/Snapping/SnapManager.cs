89a19409daf1c1c19daf10984f59097976e07ba0
Dennis Bakker <dennis--bakker@hotmail.com>
2024-03-14 13:08:03 +0100

    Snapping System

--- Assets/Scripts/Snapping/SnapManager.cs
+++ Assets/Scripts/Snapping/SnapManager.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class SnapManager : MonoBehaviour
 {
-    
+    //[Header("Part Enums")]
+    //WindmillInformation.Part bottomPart = WindmillInformation.Part.BOTTOM;
+    //WindmillInformation.Part topPart = WindmillInformation.Part.TOP;
+    //WindmillInformation.Part windmillBladePart = WindmillInformation.Part.WINDMILLBLADES;
+
+    //[Header("Era Enums")]
+    //WindmillInformation.Era traditionalEra = WindmillInformation.Era.TRADITIONAL;
+    //WindmillInformation.Era modernEra = WindmillInformation.Era.MODERN;
+
+    [Header("Variable")]
+    public bool canSnap;
+
+    public void CheckIfCanSnap(WindmillInformation windmillInformation)
+    {
+        canSnap = false;
+        switch (windmillInformation.part)
+        {
+            case WindmillInformation.Part.TOP:
+                if (WindmillInformation.Part.BOTTOM == windmillInformation.part)
+                {
+                    canSnap = true;
+                }
+                break;
+            case WindmillInformation.Part.WINDMILLBLADES:
+                if (WindmillInformation.Part.TOP == windmillInformation.part)
+                {
+                    canSnap = true;
+                }
+                break;
+            default:
+                // do this if nothing else triggers.
+                break;
+        }
+    }
 }
