09a6101b1d7b158ac83a695eb8efc7cd2498c961
MacKenzie1976 <97097945@st.deltion.nl>
2024-03-18 15:13:24 +0100

    Destroy Script

--- Assets/Scripts/Removal/PartRemoval.cs
+++ Assets/Scripts/Removal/PartRemoval.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PartRemoval : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void DestroyGameObject()
     {
-        
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
+    void DestroyScriptInstance()
+    {
+        // Removes this script instance from the game object
+        Destroy(this);
+    }
+
+    void DestroyComponent()
+    {
+        // Removes the rigidbody from the game object
+        Destroy(GetComponent<Rigidbody>());
+    }
+
+    void DestroyObjectDelayed()
+    {
+        // Kills the game object in 5 seconds after loading the object
+        Destroy(gameObject, 5);
+    }
+
+    // When the user presses Ctrl, it will remove the
+    // BoxCollider component from the game object
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && GetComponent<BoxCollider>())
+        {
+            Destroy(GetComponent<BoxCollider>());
+        }
     }
 }
---
+++ Assets/Scripts/Removal/Removal2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Removal2 : MonoBehaviour
+{
+    public GameObject part;
+
+
+
+    void Start()
+    {
+        
+    }
+    public void DestroyPart()
+    {
+        Destroy(part);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
